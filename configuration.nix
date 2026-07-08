9f0ec861a0bb4801c5945b0b9e25abffb48291ba
Richard Huynh <huynhtastic@gmail.com>
2026-07-08 15:44:35 -0500

    feat: replace all contents with nix config

---
+++ configuration.nix
@@ -0,0 +1,12 @@
+{ user, ... }:
+
+{
+  # Determinate already manages the Nix daemon, so nix-darwin shouldn't.
+  nix.enable = false;
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  system.primaryUser = user;
+  system.stateVersion = 6;
+}
